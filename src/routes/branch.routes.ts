fd013cf334964109159c2be4cca7814b525f8646
AshikChristober <ashikdchrist2004@gmail.com>
2026-07-16 20:16:51 +0530

    Make GET /branches endpoint public to allow login page to fetch branches

--- src/routes/branch.routes.ts
+++ src/routes/branch.routes.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
-router.get('/', authMiddleware, branchController.getAll);
+router.get('/', branchController.getAll);
 router.get('/:id', authMiddleware, branchController.getById);
 router.post('/', authMiddleware, branchController.create);
 router.put('/:id', authMiddleware, branchController.update);
